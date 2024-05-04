831c6160d72b0771d0c435a8be8d446d898e74a1
Jake Applehans <104577130+Applehand@users.noreply.github.com>
2024-05-04 12:21:33 -0500

    setup templating, no styles

--- app.py
+++ app.py
@@ -3,5 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template('index.html')
+def main():
+    links = {'Link 1':"/slug-1", 'Link 2':"/slug-2", 'Link 3':"/slug-3"}
+    return render_template('base.html', links=links)
