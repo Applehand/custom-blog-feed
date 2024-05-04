d40c569c13f2778d0cabab9226d2a5cb56d3467c
Jake Applehans <104577130+Applehand@users.noreply.github.com>
2024-05-04 17:36:41 -0500

    templating, basic styling, routes, form
    Setup templates using Jinja, added some bad styling, setup three routes and navigation links, created form on homepage, created simple post data

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+links = {'Home':"/", 'Blog':"/blog", 'Wtf?':"/wtf"}
 
 @app.route("/")
 def main():
-    links = {'Link 1':"/slug-1", 'Link 2':"/slug-2", 'Link 3':"/slug-3"}
-    return render_template('base.html', links=links)
+    return render_template('blog-form.html', links=links)
+
+@app.route("/blog")
+def blog():
+    return render_template('blog-feed.html', links=links)
+
+@app.route("/wtf")
+def wtf():
+    return render_template('wtf.html', links=links)
+
+@app.route("/blog/<int:blog_id>/<blog_title>")
+def blog_post(blog_id, blog_title):
+    return f"BlogID: {blog_id} - Title: {blog_title}"
