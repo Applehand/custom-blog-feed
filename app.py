af8bc625b21d0f1684dfb9bc988d2ac1696b783d
Jake Applehans <104577130+Applehand@users.noreply.github.com>
2024-05-05 19:56:12 -0500

    home, feed, and post view 1st drafts
    Test json posts, more templating, dynamic post route, hardcoded nav links

--- app.py
+++ app.py
@@ -1,20 +1,38 @@
 from flask import Flask, render_template
+import os
+import json
 
 app = Flask(__name__)
-links = {'Home':"/", 'Blog':"/blog", 'Wtf?':"/wtf"}
+# links = {'Home':"/", 'Blog':"/blog", 'Wtf?':"/wtf"}
 
 @app.route("/")
 def main():
-    return render_template('blog-form.html', links=links)
+    return render_template('blog-form.html')
 
 @app.route("/blog")
 def blog():
-    return render_template('blog-feed.html', links=links)
+    posts = get_blog_posts()
+    return render_template('blog-feed.html', posts=posts)
 
 @app.route("/wtf")
 def wtf():
-    return render_template('wtf.html', links=links)
+    return render_template('wtf.html')
 
-@app.route("/blog/<int:blog_id>/<blog_title>")
-def blog_post(blog_id, blog_title):
-    return f"BlogID: {blog_id} - Title: {blog_title}"
+@app.route("/blog/<int:blog_id>")
+def blog_post(blog_id):
+    posts = get_blog_posts()
+    for post in posts:
+        id = int(post['id'])
+        if id == blog_id:
+            return render_template(f'blog-post.html', post=post)
+    return "Blog ain't here, man."
+
+def get_blog_posts():
+    posts = []
+    posts_directory = os.path.join(app.root_path, 'posts')
+    for filename in os.listdir(posts_directory):
+        if filename.endswith('.json'):
+            with open(os.path.join(posts_directory, filename), 'r') as file:
+                post_data = json.load(file)
+                posts.append(post_data)
+    return posts
